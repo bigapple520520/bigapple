a313c8ff32a2ec988896e6025368f863bb24ab28
bigapple520520 <xuan@xuantekiMacBook-Pro.local>
2013-12-29 17:36:30 +0800

    20131229

--- trunk/bigapple/gen/com/winupon/andframe/bigapple/R.java
+++ trunk/bigapple/gen/com/winupon/andframe/bigapple/R.java
@@ -23,7 +23,7 @@ public final class R {
         public static final int menu_settings=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int bigapple_main=0x7f030000;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
