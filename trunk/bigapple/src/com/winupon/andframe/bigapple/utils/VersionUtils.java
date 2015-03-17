8318078cb7285a9416ca1c2d099fb385a668b26c
xuan <15858178400@163.com>
2015-03-17 15:56:33 +0800

    xuan

--- trunk/bigapple/src/com/winupon/andframe/bigapple/utils/VersionUtils.java
+++ trunk/bigapple/src/com/winupon/andframe/bigapple/utils/VersionUtils.java
@@ -18,11 +18,33 @@ import android.content.pm.PackageManager.NameNotFoundException;
 public abstract class VersionUtils {
 
     /**
-     * 得到版本代码versionCode（配在AndroidManifest.xml中），主版本号，用于升级应用
+     * 得到版本代码versionCode（配在AndroidManifest.xml中），主版本号，用于升级应用<br>
+     * 例如：android:versionCode="1"
      * 
      * @param context
      * @return
      */
+    public static int getVersionCode(Context context) {
+        int versionCode = -1;
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            versionCode = packageInfo.versionCode;
+        }
+        catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return versionCode;
+    }
+
+    /**
+     * 得到版本代码versionCode（配在AndroidManifest.xml中），主版本号，用于升级应用<br>
+     * 使用：getVersionCode(Context context)更方便
+     * 
+     * @param context
+     * @return
+     */
+    @Deprecated
     public static int getVersionCode(Context context, Class<?> clazz) {
         int versionCode = -1;
         try {
@@ -37,10 +59,31 @@ public abstract class VersionUtils {
     }
 
     /**
-     * 得到版本代码versionName（配在AndroidManifest.xml中），给用户看的
+     * 得到版本代码versionName（配在AndroidManifest.xml中），给用户看的<br>
+     * 例如：android:versionName="1.0"
+     * 
+     * @param context
+     * @return
+     */
+    public static String getVersionName(Context context) {
+        String versionName = "";
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            versionName = packageInfo.versionName;
+        }
+        catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionName;
+    }
+
+    /**
+     * 得到版本代码versionName（配在AndroidManifest.xml中），给用户看的<br>
+     * 使用：getVersionName(Context context)更方便
      * 
      * @return
      */
+    @Deprecated
     public static String getVersionName(Context context, Class<?> clazz) {
         String versionName = "";
         try {
@@ -54,11 +97,32 @@ public abstract class VersionUtils {
     }
 
     /**
-     * 获得应用程序图标
+     * 获得应用程序图标<br>
+     * android:icon="@drawable/ic_launcher"
      * 
      * @param context
      * @return
      */
+    public static int getApplicationIcon(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.applicationInfo.icon;
+        }
+        catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+
+    /**
+     * 获得应用程序图标<br>
+     * 使用：getApplicationIcon(Context context)更方便
+     * 
+     * @param context
+     * @return
+     */
+    @Deprecated
     public static int getApplicationIcon(Context context, Class<?> clazz) {
         try {
             PackageInfo packageInfo = context.getPackageManager().getPackageInfo(clazz.getPackage().getName(), 0);
